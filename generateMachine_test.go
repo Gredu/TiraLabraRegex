e620d831fd79451f00c45a95467795fe11f4a206
Greatman Lim <greatman.lim@mahti.org>
2017-09-29 20:26:04 +0300

    Add generateMachine test

--- generateMachine_test.go
+++ generateMachine_test.go
@@ -1,28 +1,28 @@
 package main
 
-// func TestGenerateMachine {
-//
-// }
-//
-// type testvalues struct {
-// 	machines []State
-// 	expectedValues []State
-// }
-//
-// type parsedtokens struct {
-// 	tokens []Token
-// }
-//
-// var tokens = []{
-// 	{
-// 		parseRegexp("abc")
-// 	},
-// }
-//
-// var tests = []testvalues{
-// 	{
-// 		generateMachine("abc"),
-//
-// 	}
-// }
-//
+import "testing"
+
+func TestGenerateMachine(t *testing.T) {
+
+	var currentState = generateMachine(parseRegexp("abc"))
+
+	testCheckHelper(t, currentState, false, "a", "literal")
+	currentState = *currentState.transitions[0].state
+
+	testCheckHelper(t, currentState, false, "b", "literal")
+	currentState = *currentState.transitions[0].state
+
+	testCheckHelper(t, currentState, true, "c", "literal")
+}
+
+func testCheckHelper(t *testing.T, s State, expectedBool bool, expectedTokenValue string, expectedTokenOperator string) {
+	if s.accept {
+		t.Error("Expected", false, "but got", s.accept)
+	}
+	if s.transitions[0].token.value != expectedTokenValue {
+		t.Error("Expected", expectedTokenValue, "but got", s.transitions[0].token.value)
+	}
+	if s.transitions[0].token.typeOperator != expectedTokenOperator {
+		t.Error("Expected", expectedTokenOperator, "but got", s.transitions[0].token.typeOperator)
+	}
+}
