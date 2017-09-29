7da8ab5882c719717451148724dcb41c0920c0e0
Greatman Lim <greatman.lim@mahti.org>
2017-09-29 18:01:40 +0300

    Refract

--- regexp_test.go
+++ regexp_test.go
@@ -33,3 +33,30 @@ var tests = []testvalues{
 		[]string{"literal", "literal", "literal"},
 	},
 }
+
+// func TestGenerateMachine {
+//
+// }
+//
+// type testvalues struct {
+// 	machines []State
+// 	expectedValues []State
+// }
+//
+// type parsedtokens struct {
+// 	tokens []Token
+// }
+//
+// var tokens = []{
+// 	{
+// 		parseRegexp("abc")
+// 	},
+// }
+//
+// var tests = []testvalues{
+// 	{
+// 		generateMachine("abc"),
+//
+// 	}
+// }
+//
