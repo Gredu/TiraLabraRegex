d746dac39f6f3a46cf593f7bf63ad774489f389e
Greatman Lim <greatman.lim@mahti.org>
2017-10-01 20:25:11 +0300

    Refract

--- generateMachine_test.go
+++ generateMachine_test.go
@@ -7,16 +7,47 @@ func TestGenerateMachine(t *testing.T) {
 	var currentState State = generateMachine(parseRegexp("abc"))
 	expectedValues := []string{"a", "b", "c"}
 
-	for i := 0; i < 2; i++ {
-
-		if currentState.accept {
-			t.Error("Expected", false, "but got", currentState.accept)
+	for i := 0; i < 3; i++ {
+		if i == 2 {
+			if currentState.transitions[0].token.value != "c" {
+				t.Error("Expected last state's token value to be 'c', but got", currentState.transitions[0].token.value)
+			}
+			// if !currentState.accept {
+			// 	t.Error("Expected last state's accept to be true, but got", currentState.accept)
+			// }
+		} else {
+			if currentState.accept {
+				t.Error("Expected", false, "but got", currentState.accept)
+			}
 		}
 		if currentState.transitions[0].token.value != expectedValues[i] {
 			t.Error("Expected", expectedValues[i], "but got", currentState.transitions[0].token.value)
 		}
 		if currentState.transitions[0].token.typeOperator != "literal" {
 			t.Error("Expected literal but got", currentState.transitions[0].token.typeOperator)
+		}
+
+		currentState = *currentState.transitions[0].state
+	}
+
+	currentState = generateMachine(parseRegexp("ab*c"))
+	expectedValues = []string{"a", "b"}
+
+	for i := 0; i < 2; i++ {
+		if currentState.accept {
+			t.Error("Expected", false, "but got", currentState.accept)
+		}
+		if currentState.transitions[0].token.value != expectedValues[i] {
+			t.Error("Expected", expectedValues[i], "but got", currentState.transitions[0].token.value)
+		}
+		if i == 1 {
+			if currentState.transitions[0].token.typeOperator != "star" {
+				t.Error("Expected literal but got", currentState.transitions[0].token.typeOperator)
+			}
+		} else {
+			if currentState.transitions[0].token.typeOperator != "literal" {
+				t.Error("Expected literal but got", currentState.transitions[0].token.typeOperator)
+			}
 		}
 
 		currentState = *currentState.transitions[0].state
