4d47d3166abaef84dd6436a9575ed5225d131c4e
Greatman Lim <greatman.lim@mahti.org>
2017-10-01 19:46:01 +0300

    Refract

--- generateMachine_test.go
+++ generateMachine_test.go
@@ -4,25 +4,21 @@ import "testing"
 
 func TestGenerateMachine(t *testing.T) {
 
-	var currentState = generateMachine(parseRegexp("abc"))
+	var currentState State = generateMachine(parseRegexp("abc"))
+	expectedValues := []string{"a", "b", "c"}
 
-	testCheckHelper(t, currentState, false, "a", "literal")
-	currentState = *currentState.transitions[0].state
+	for i := 0; i < 2; i++ {
 
-	testCheckHelper(t, currentState, false, "b", "literal")
-	currentState = *currentState.transitions[0].state
+		if currentState.accept {
+			t.Error("Expected", false, "but got", currentState.accept)
+		}
+		if currentState.transitions[0].token.value != expectedValues[i] {
+			t.Error("Expected", expectedValues[i], "but got", currentState.transitions[0].token.value)
+		}
+		if currentState.transitions[0].token.typeOperator != "literal" {
+			t.Error("Expected literal but got", currentState.transitions[0].token.typeOperator)
+		}
 
-	testCheckHelper(t, currentState, true, "c", "literal")
-}
-
-func testCheckHelper(t *testing.T, s State, expectedBool bool, expectedTokenValue string, expectedTokenOperator string) {
-	if s.accept {
-		t.Error("Expected", false, "but got", s.accept)
-	}
-	if s.transitions[0].token.value != expectedTokenValue {
-		t.Error("Expected", expectedTokenValue, "but got", s.transitions[0].token.value)
-	}
-	if s.transitions[0].token.typeOperator != expectedTokenOperator {
-		t.Error("Expected", expectedTokenOperator, "but got", s.transitions[0].token.typeOperator)
+		currentState = *currentState.transitions[0].state
 	}
 }
