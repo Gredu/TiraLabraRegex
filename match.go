b95effc0d364780146331573f401a950c2f57e24
Greatman Lim <greatman.lim@mahti.org>
2017-10-07 10:13:10 +0300

    Fix out of range recursion

--- match.go
+++ match.go
@@ -4,19 +4,18 @@ var accepted bool = false
 
 func match(input string, currentState State) bool {
 
-	for _, transition := range currentState.transitions {
-		switch transition.token.typeOperator {
-		case "literal":
-			if input[0:1] == transition.token.value {
-				if len(input) == 1 && transition.state.accept {
-					accepted = true
-				} else {
-					accepted = false
+	if len(input) == 0 && len(currentState.transitions) == 0 {
+		return true
+	} else if len(input) > 0 && len(currentState.transitions) > 0 {
+		for _, transition := range currentState.transitions {
+			switch transition.token.typeOperator {
+			case "literal":
+				if input[0:1] == transition.token.value {
+					return match(input[1:], *transition.state)
 				}
-				return match(input[1:], *transition.state)
 			}
 		}
 	}
 
-	return accepted
+	return false
 }
