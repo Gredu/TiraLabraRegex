cc96c4cb764ae5b73072766375e9d1347b3b4ba2
Greatman Lim <greatman.lim@mahti.org>
2017-10-07 12:16:03 +0300

    Add meta \d match

--- match.go
+++ match.go
@@ -15,9 +15,24 @@ func match(input string, currentState State) bool {
 				if input[0:1] == transition.token.value {
 					return match(input[1:], *transition.state)
 				}
+			case "meta":
+				switch transition.token.value {
+				case "d":
+					if isDigit(input[0:1]) {
+						return match(input[1:], *transition.state)
+					}
+				}
 			}
 		}
 	}
 
+	return false
+}
+
+func isDigit(input string) bool {
+	switch input {
+	case "1", "2", "3", "4", "5", "6", "7", "8", "9", "0":
+		return true
+	}
 	return false
 }
