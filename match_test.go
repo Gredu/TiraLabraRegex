dbf16d15e508b68a81773ee1424457de89bee1fa
Greatman Lim <greatman.lim@mahti.org>
2017-10-07 10:12:17 +0300

    Add more simple literal tests

--- match_test.go
+++ match_test.go
@@ -6,6 +6,15 @@ func TestMatch(t *testing.T) {
 
 	machine := generateMachine(parseRegexp("abc"))
 
+	if match("", machine) {
+		t.Error("Word '' should not match, but it did")
+	}
+	if match("a", machine) {
+		t.Error("Word 'a' should not match, but it did")
+	}
+	if match("ab", machine) {
+		t.Error("Word 'ab' should not match, but it did")
+	}
 	if !match("abc", machine) {
 		t.Error("Word 'abc' should match, but did not")
 	}
