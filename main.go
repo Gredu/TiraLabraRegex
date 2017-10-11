e90c1cca8d2928d0f4c4154f532baaa94d15c023
Greatman Lim <greatman.lim@mahti.org>
2017-10-11 14:17:12 +0300

    Read from file with regexp

--- main.go
+++ main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
 
+	fi, err := os.Stdin.Stat()
+	if err != nil {
+		panic(err)
+	}
+	if fi.Mode()&os.ModeNamedPipe == 0 {
+		inputs := os.Args[1]
+		regexp := os.Args[2]
+
+		machine := generateMachine(parseRegexp(regexp))
+
+		matchLine(inputs, machine)
+
+	} else {
+		// TODO
+		fmt.Println("hi pipe!")
+	}
+
+}
+
+func matchLine(path string, machine State) {
+	inFile, _ := os.Open(path)
+	defer inFile.Close()
+	scanner := bufio.NewScanner(inFile)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		if match(scanner.Text(), machine) {
+			fmt.Println(scanner.Text())
+		}
+	}
 }
